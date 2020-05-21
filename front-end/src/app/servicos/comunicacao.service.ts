a65cf86581e6934d1692a22d1c947c0176a9a656
adalcinojunior <adaljunior.inf@gmail.com>
2020-05-21 17:00:39 -0300

    Segundo commit da feature

--- front-end/src/app/servicos/comunicacao.service.ts
+++ front-end/src/app/servicos/comunicacao.service.ts
@@ -4,16 +4,14 @@ import { Injectable, EventEmitter } from '@angular/core';
 @Injectable()
 export class ComunicacaoService {
     alocacao = new EventEmitter();
-    
-    constructor() {}
 
     alocacaoSalva() {
         this.alocacao.emit('Alocação salva!');
     }
-    
+
     alocacaoDevolvida() {
         this.alocacao.emit('Alocação devolvida!');
     }
 
-    
+
 }
